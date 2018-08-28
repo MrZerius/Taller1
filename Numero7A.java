501ec5e72635e1498756dcda354b35a5772d01fb
MrZerius <33948567+MrZerius@users.noreply.github.com>
2018-08-28 09:48:05 -0500

    Update Numero7A.java

--- Numero7A.java
+++ Numero7A.java
@@ -1,5 +1,29 @@
 package Numero7;
 
+import java.util.Scanner;
+
 public class Numero7A {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+	
+	int num,i,result;
+
+	System.out.println("Ingrese el numero del cual desea saber la tabla");
+	
+	num = sc.nextInt();
+	
+	System.out.println("Tabla de multiplicacion");
+	for(i=1; i<=12; i++) {
+		
+		result = num * i;
+		
+		System.out.println(+num+ " x " +i+ " = " +result++);
+		
+		
+	}
+	sc.close();
+	
+
+}
 
 }
