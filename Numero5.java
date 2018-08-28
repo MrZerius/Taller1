fb800fa4689400d25217bf290307276193b23e5f
MrZerius <33948567+MrZerius@users.noreply.github.com>
2018-08-28 09:33:18 -0500

    Update Numero5.java

--- Numero5.java
+++ Numero5.java
@@ -1,5 +1,28 @@
 package Numero5;
 
+import java.util.Scanner;
+
 public class Numero5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+	
+	int km;
+	double ms;
+	System.out.println("Ingrese la velocidad en k/m ");
+	
+	km = sc.nextInt();
+	
+	ms =  km * (1000/3600) ;
+	
+	
+	System.out.println(ms = km * 1000/3600);
+	
+	System.out.println( + km + " km/h son " + ms + " m/s");
+	
+	
+	sc.close();
+	
+
+}
 
 }
