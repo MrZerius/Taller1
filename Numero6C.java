6bd7fe97cbf929ec4bc739a3e75f5e6a6af1abb5
MrZerius <33948567+MrZerius@users.noreply.github.com>
2018-08-28 09:41:23 -0500

    Update Numero6C.java

--- Numero6C.java
+++ Numero6C.java
@@ -1,5 +1,27 @@
 package Numero6;
 
+import java.util.Scanner;
+
 public class Numero6C {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+	
+	int num = 1;
+	int cont = 0;
+	
+	while(num < 50) {
+		
+		cont = cont + num;
+		num = num +2;
+	}
+	
+	
+	System.out.println("La suma de los numeros impares del 1 al 49 es : "+cont++);
+	
+	
+	sc.close();
+	
+
+}
 
 }
