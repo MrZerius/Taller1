7e944ea4ba6886d92a8e1561311bcad0308666fb
MrZerius <33948567+MrZerius@users.noreply.github.com>
2018-08-28 09:37:39 -0500

    Update Numero6A.java

--- Numero6A.java
+++ Numero6A.java
@@ -1,5 +1,27 @@
 package Numero6;
 
+import java.util.Scanner;
+
 public class Numero6A {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+	
+	int num = 1;
+	int cont = 0;
+	
+	while(num <= 100) {
+		
+		cont = cont + num;
+		num = num +1;
+	}
+	
+	
+	System.out.println("La suma de los numeros del 1 al 100 es: "+cont++);
+	
+	
+	sc.close();
+	
+
+}
 
 }
