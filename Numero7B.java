5d9d646b37d4da9ddda61aa90d1ca7a07727c258
MrZerius <33948567+MrZerius@users.noreply.github.com>
2018-08-28 09:56:03 -0500

    Update Numero7B.java

--- Numero7B.java
+++ Numero7B.java
@@ -1,5 +1,27 @@
 package Numero7;
 
+import java.util.Scanner;
+
 public class Numero7B {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+	
+	int num,i,result=1;
+
+	System.out.println("Ingrese el numero el cual desea factorizar:");
+	
+	num = sc.nextInt();
+	
+	System.out.println("Factorizacion");
+	for(i=1; i<=num; i++) {
+		
+		result = result * i;
+	}
+	
+	System.out.println("El factorial de " +num+ " es " +result++);
+	sc.close();
+	
+
+}
 
 }
