d9f5a3a1349d0befd359b136327d5ed94b590725
MrZerius <33948567+MrZerius@users.noreply.github.com>
2018-08-28 09:40:08 -0500

    Update Numero6B.java

--- Numero6B.java
+++ Numero6B.java
@@ -1,5 +1,27 @@
 package Numero6;
 
+import java.util.Scanner;
+
 public class Numero6B {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+	
+	int num = 2;
+	int cont = 0;
+	
+	while(num <= 50) {
+		
+		cont = cont + num;
+		num = num +2;
+	}
+	
+	
+	System.out.println("La suma de los numeros pares del 2 al 50 es : "+cont++);
+	
+	
+	sc.close();
+	
+
+}
 
 }
